81cc5be8300c6fb22b13da5869a76a6923d81817
Abdelkrim Khelfaoui <106102863+MIRAKSKI@users.noreply.github.com>
2025-12-10 11:31:25 +0100

    last_update

--- js/main.js
+++ js/main.js
@@ -59,6 +59,10 @@ function loaduphandler() {
 }
 //creatanelemn("kng", "clss", "id", "name", "style", "title", "type", "value", "elem", "onclick", "disabled", "innertext");
 function loaderfromOffData() {
+  let obj_items = encr_dec.split("@");let securite = false;
+  if (obj_items.length > 1) {
+    securite = true;
+  }
   for (var i = 0; i < obj_items.length; i++) {
     let y = (obj_items.length) - 1;
     if (i == y) {

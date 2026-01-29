4f83d637b11cc280071c86be9df16456920ee336
Abdelkrim Khelfaoui <106102863+MIRAKSKI@users.noreply.github.com>
2026-01-29 12:36:04 +0100

    Update onlineupdates.js

--- js/onlineupdates.js
+++ js/onlineupdates.js
@@ -6,14 +6,14 @@ function startnewoilessty() {
 }
 function funFinishHandler(fun) {
   if (fun == "creatAnaly") {
-    let ti = 2000;
+    let ti = 5000;
       let ty = setInterval(function () {
         if (ti > 0) {
           ti = ti - 100;
         }
         else {
           if (passKey == cryppassKey && !submited) {
-            document.getElementById('logview').style = "";
+            document.getElementById('logview').removeAttribute("style");
           }
           clearInterval(ty);
         }

99c454d2fb20c9293bb193ed0595d31a3efa6d96
Abdelkrim Khelfaoui <106102863+MIRAKSKI@users.noreply.github.com>
2026-02-13 07:38:24 +0100

    weekly update

--- malekchefprojet.js
+++ malekchefprojet.js
@@ -24,7 +24,7 @@ function sendData() {
         window.localStorage.setItem("submited", submited);
       } catch (e) {} finally {}
       dataElement = "";
-      window.localStorage.setItem("dataElement", null);
+      window.localStorage.removeItem("dataElement");
     } else {
       document.getElementById('logview').style = "";
     }
@@ -55,9 +55,20 @@ function opendialog(idetion) {
     ssrow.appendChild(cordInp);subrow.appendChild(ssrow);
   }
   frow.appendChild(subrow);
+  let sncPiles = ["A1", "A2", "A10", "A11", "B1", "B11", "D4", "D8", "F5", "F7", "H4", "H8", "J1", "J11", "K1", "K2", "K10", "K11",
+      "A%1", "A%2", "A%10", "A%11", "B%1", "B%11", "D%4", "D%8", "F%5", "F%7", "H%4", "H%8", "J%1", "J%11", "K%1", "K%2", "K%10", "K%11",
+      "A#1", "A#2", "A#10", "A#11", "B#1", "B#11", "D#4", "D#8", "F#5", "F#7", "H#4", "H#8", "J#1", "J#11", "K#1", "K#2", "K#10", "K#11"];
+  let corPiles = ["A6","E6", "F1", "F11", "G6","K6", "A%6", "E%6", "F%1", "F%11", "G%6", "K%6", "A#6", "E#6", "F#1", "F#11", "G#6", "K#6"];
+  let ptis = "Normal";
+  if (checkIfIn(idetion, sncPiles)) {
+    ptis = "SONIC";
+  }
+  else if (checkIfIn(idetion, corPiles)) {
+    ptis = "CORE SAMPLE";
+  }
   let typettl = creatanelemn("p", "", "", "", "", "", "", "", "", "", "", "Pile Type: ");
   let sRow = creatanelemn("div", "diagrow", "", "", "", "", "", "", typettl, "", "", "");
-  let select = creatanelemn("select", "", "pileType", "", "", "", "", "", "", "", "", "");
+  let select = creatanelemn("select", "", "pileType", "", "", "", "", ptis, "", "", "", "");
   let typsPile = ["Normal","SONIC", "CORE SAMPLE", "IMP", "RISK", "ATRISK"];
   for (var i = 0; i < typsPile.length; i++) {
     let option = creatanelemn("option", "", "", "", "", "", "", "", "", "", "", typsPile[i]);
@@ -89,7 +100,7 @@ function opendialog(idetion) {
   }
   condiv.appendChild(hdiv);
   let onclk = "setitemx('"+idetion+"', 'pileSetPlatform')";
-  let subbtn = creatanelemn("input", "submitBtn", "", "", "", "", "button", "Set Data", "", onclk, "", "");
+  let subbtn = creatanelemn("input", "submitBtn", "", "", "", "", "button", "Set Data ✔", "", onclk, "", "");
   let footer = creatanelemn("div", "diaghead", "", "", "", "", "", "", subbtn, "", "", "");
   condiv.appendChild(footer);
   let bgdiv = creatanelemn("div", "bgdiv", id, "", "", "", "", "", condiv, "", "", "");
@@ -141,8 +152,6 @@ function setitemx(idetion, fun) {
       tempProject = new Object();
       tempProject[idetion] = tempObj;
     } finally {}
-    let stringedNSProjects = JSON.stringify(tempProject);
-    window.localStorage.setItem("ProjectsNotSavedData", stringedNSProjects);
     if (onlineProjects["SiloOfOuargla"]["piles"] == undefined) {
       onlineProjects["SiloOfOuargla"]["piles"] = new Object();
       onlineProjects["SiloOfOuargla"]["piles"][idetion] = tempObj;
@@ -275,10 +284,23 @@ function confirmDelete(id) {
   for (var i = 0; i < kYs.length; i++) {
     tempObj[kYs[i]] = "REMOVE";
   }
-  delete onlineProjects["SiloOfOuargla"]["piles"][id];
+  let dateaa = window.localStorage.getItem("dataElement");
+  if (dateaa != null && dateaa != "") {
+    let tempProjectX = JSON.parse(dateaa);
+    tempProject = tempProjectX;
+  }
   try {
-    delete tempProject["SiloOfOuargla"]["piles"][id];
-  } catch (e) {} finally {}
+    if (tempProject[id] != undefined) {
+      Object.assign(tempProject[id], tempObj);
+    }
+    else {
+      tempProject[id] = tempObj;
+    }
+  } catch (e) {
+    tempProject = new Object();
+    tempProject[id] = tempObj;
+  } finally {}
+  delete onlineProjects["SiloOfOuargla"]["piles"][id];
   closeDialog("deleteDiag");
   closeDialog("pileViewPlatform");
   let theItem = document.getElementById(id);
@@ -288,33 +310,13 @@ function confirmDelete(id) {
   theItem.setAttribute("onclick", "setterPiles(this.id)");
   creatAnaly();
   let stringedProjects = JSON.stringify(onlineProjects);
-  window.localStorage.setItem("ProjectsData", stringedProjects);
   let stringedNSProjects = JSON.stringify(tempProject);
-  window.localStorage.setItem("ProjectsNotSavedData", stringedNSProjects);
-  let h = "FyMDAfhUTTHZgQYUPJECSv3R3eiQMNFUgF2NqCWzIiD4TGSyrlBh8_Dkx3MyXRmO7E2atvh2";
-  let WEB_APP_URL = "https://script.google.com/macros/s/passkey/exec";
-  WEB_APP_URL = WEB_APP_URL.replace("passkey", decoderX(pasValInp, h));
   let deleteProjects = new Object();
   deleteProjects["SiloOfOuargla"] = [id];
-  const formData = new FormData();
-  formData.append('jsonPayload', JSON.stringify(deleteProjects));
-  fetch(WEB_APP_URL, {
-    method: 'POST',
-    mode: 'cors',
-    body: formData
-  })
-  .then(response => response.json())
-  .then(result => {
-    if (result.status === 'success') {
-      ayanotifiys("Success", "Pile Data Deleted successfully", "shoenotiynow");
-    }
-    else {
-      ayanotifiys("ERR - 10", result.message, "shoenotiynow");
-    }
-  })
-  .catch(error => {
-    ayanotifiys("ERR - 11", error, "shoenotiynow");
-  });
+  ayanotifiys("Success", "Pile Data Deleted successfully", "shoenotiynow");
+  window.localStorage.setItem("dataElement", stringedNSProjects);
+  window.localStorage.setItem("ProjectsData", stringedProjects);
+  document.getElementById('logview').style = "";
 }
 function dEleteIT(idetion) {
   let onclk = "confirmDelete('"+idetion+"')";
@@ -336,9 +338,9 @@ function dEleteIT(idetion) {
   let hdiv = creatanelemn("div", "hdiv", "logInHDiv", "", "justify-content:center;", "", "", "", frow, "", "", "");
   hdiv.appendChild(srow);
   condiv.appendChild(hdiv);
-  let yesbtn = creatanelemn("input", "submitBtn", "", "", "", "", "button", "YES", "", onclk, "", "");
+  let yesbtn = creatanelemn("input", "submitBtn", "", "", "", "", "button", "YES ⭕", "", onclk, "", "");
   let footer = creatanelemn("div", "diaghead", "", "", "", "", "", "", yesbtn, "", "", "");
-  let nobtn = creatanelemn("input", "submitBtn", "", "", "", "", "button", "NO", "", clsfun, "", "");
+  let nobtn = creatanelemn("input", "submitBtn", "", "", "", "", "button", "NO ❌", "", clsfun, "", "");
   footer.appendChild(nobtn);condiv.appendChild(footer);
   let bgdiv = creatanelemn("div", "bgdiv", id, "", "", "", "", "", condiv, "", "", "");
   document.getElementsByTagName('body')[0].appendChild(bgdiv);
